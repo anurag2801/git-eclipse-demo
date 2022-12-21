d3fe992fe4d8be954582599fb81509db200b9c45
Vaibhav <vaibhavthorat2552@gmail.com>
2022-12-21 11:42:58 +0530

    stage2 push

---
+++ gitdemo/src/com/gitdemo/demo2anurag.java
@@ -0,0 +1,11 @@
+package com.gitdemo;
+
+public class demo2anurag {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Stage 2 push");
+
+	}
+
+}
