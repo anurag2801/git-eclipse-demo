403d61cd80685d46aeb34081afa82bb117f02e6f
Anurag <anuragab28@gmail.com>
2022-12-21 11:31:08 +0530

    new class is added

---
+++ gitdemo/src/com/gitdemo/git.java
@@ -0,0 +1,23 @@
+package com.gitdemo;
+
+public class git {
+	
+	
+	private int git;
+
+	public git(int git) {
+		super();
+		this.git = git;
+	}
+
+	public int getGit() {
+		return git;
+	}
+
+	public void setGit(int git) {
+		this.git = git;
+	}
+	
+	
+
+}
