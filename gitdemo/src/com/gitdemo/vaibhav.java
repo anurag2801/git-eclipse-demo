5a60bae5c057c32de7e98bd5343a0333457a9f73
Vaibhav <vaibhavthorat2552@gmail.com>
2022-12-21 11:37:23 +0530

    push to anurag

---
+++ gitdemo/src/com/gitdemo/vaibhav.java
@@ -0,0 +1,10 @@
+package com.gitdemo;
+
+public class vaibhav {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("push to anurag github");
+	}
+
+}
